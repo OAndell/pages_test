2b1a976be41b293dad81af1a7d08109f98984353
Oscar Andell <oscar@andell.eu>
2018-12-10 08:26:44 +0100

    first

---
+++ src/felo_z.js
@@ -0,0 +1,156 @@
+
+
+
+var felo_z_journal = `
+<p><strong>Stambok:</strong>Zangersheide <strong>Reg nr:</strong> 056015Z55628015<strong> Chip nr:</strong>
+528210004326910 </p>
+<p><strong>Född:</strong> 2015 <strong>Kön:</strong> Valack</p>
+<p><strong>Härstamning:</strong>  U. Violet Lady		E Fernando H</p>
+<p><strong>Uppfödare:</strong> Litjens J.A.G.P</p>
+<p><strong>Ägare:</strong> Sofia Bengtsberg</p>
+<p><strong>Temp:</strong> 37,7 <strong>Vikt:</strong> 560 kg <strong>Mankhöjd:</strong> 169cm</p>
+<p><strong>Box:</strong> 20 <strong>Hage:</strong> 21</p>`
+
+var felo_z_foderstat = `
+<h3>Morgon</h3>
+<p>  2 kg hö	1/2 skopa Pavo Podo grow,  ½ Lucern	1 dl mineraler</p>
+<h3>Lunch</h3>
+<p> - </p>
+<h3>Eftermiddag</h3>
+<p> 3 kg hö		1/2 skopa Pavo Podo grow,  ½ Lucern</p>
+<h3>Kväll</h3>
+<p> 5 kg hö</p>
+`
+
+var felo_z_gear = `
+<p>Antares träns med ergonomiskt nackstycke, låg nosgrimma, trustbett eller eggbett bett, Antares sadel, alltid skydd fram.</p>
+
+`
+
+var felo_z_hov_hist = `
+<table>
+        <tr>
+            <th>Datum</th>
+            <th>Kommentar</th>
+        </tr>
+        <tr>
+            <td>29/8</td>
+            <td>Skoning normal fram, verkning runt om <strong>(skickad till ekonomisk rapport/kostnad)</strong></td>
+        </tr>
+        <tr>
+            <td>8/10</td>
+            <td> Normal skoning med sulor fram. <strong>(skickad till ekonomisk rapport/kostnad)</strong></td>
+        </tr>
+        <tr>
+            <td>16/11</td>
+            <td> </td>
+        </tr>
+    </table>
+`
+var felo_z_vet_hist = `
+<table>
+<tr>
+    <th>Datum</th>
+    <th>Kommentar</th>
+</tr>
+
+</table>`
+
+var felo_z_beh_hist = `
+<table>
+        <tr>
+            <th>Datum</th>
+            <th>Kommentar</th>
+        </tr>
+    </table>`
+
+var felo_z_med_hist = `
+<table>
+        <tr>
+            <th>Datum</th>
+            <th>Kommentar</th>
+        </tr>
+        <tr>
+            <td>5/12</td>
+            <td> 17 Vaccination infl,  <strong>(skickad till ekonomisk rapport/kostnad)</strong></td>
+        </tr>
+        <tr>
+            <td>5/12-18 </td>
+            <td> Påminnelse om att vaccination bör ske senast detta datum, <span class="warning">Påminelse tillagd</spn>
+            </td>
+        </tr>
+        <tr>
+            <td>15/4</td>
+            <td>Avmaskning Ivomectin,  <strong>(skickad till ekonomisk rapport/kostnad)</strong></td>
+        </tr>
+    </table>
+`
+
+var felo_z_train_hist = `<table id="training_table">
+<tr>
+    <th>Datum</th>
+    <th>Typ</th>
+    <th>Kommentar</th>
+    <th>Träningsfilmer</th>
+</tr>
+<tr>
+    <td>12/11</td>
+    <td>Tömkört</td>
+    <td>Arbetade fint</td>
+    <td>
+        <ul>
+            <li><a href="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Horse-and-pony.jpg/1920px-Horse-and-pony.jpg">felo_z_träning_12/11_1</a></li>
+            <li><a href="https://upload.wikimedia.org/wikipedia/commons/3/3b/Foal-three-hours-old.jpeg">felo_z_träning_12/11_2</a></li>
+            </li>
+        </ul>
+    </td>
+</tr>
+<tr>
+    <td>13/11</td>
+    <td>Hoppträning för Svante.</td>
+    <td>Ghassan red. Arbetade med galopp byten och rakriktning. Arbetade bra.</td>
+    <td>
+
+    </td>
+</tr>
+<tr>
+    <td>14/11</td>
+    <td>Skrittmaskinen</td>
+    <td></td>
+    <td>
+        <ul>
+            <li><a href="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Horse-and-pony.jpg/1920px-Horse-and-pony.jpg">felo_z_träning_14/11</a></li>
+        </ul>
+    </td>
+</tr>
+<tr>
+    <td>15/11 </td>
+    <td>Tömkört</td>
+    <td> Arbetade bra, liksidig</td>
+    <td>
+
+    </td>
+</tr>
+</table>`
+
+var felo_z_doc = `
+`
+
+var felo_z_rap = `
+<table>
+    <tr>
+        <th>Datum</th>
+        <th>Info</th>
+        <th>Kostnad</th>
+        <th>Dokument</th>
+    </tr>
+    <td>12/12</td>
+    <td>Normal skoning med sulor fram</td>
+    <td>1875:-</td>
+    <td><a href="https://google.com">Ladda ner PDF</a></td>
+    <tr>
+</table>
+`
+
+
+
